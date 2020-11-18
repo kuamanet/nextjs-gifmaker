ac0df048acf4b715c267e19bcdc1565ac593cb0e
Gezim <gezim@kuama.net>
2020-11-18 17:52:26 +0100

    converting a video into a gif

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,71 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+import { useEffect, useState } from 'react';
+
+const ffmpeg = createFFmpeg({ log: true });
 
 export default function Home() {
-  return (
+
+  const [ready, setReady] = useState();
+  const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
+
+
+  const convertToGif = async () => {
+    // Write the file to memory 
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+    // Run the FFMpeg command
+    // '-i' is for the input file, the '-t' is for the time of the video to be, '-ss' starting seconds
+    // '-f' we use it to encode that is a gif file
+    await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+    // Read the result
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    // Create a URL
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+
+    setGif(url);
+  }
+
+  return ready ? (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Convert a video into a Gif</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1>
+          Convert a video into a gif
         </h1>
+        <div className="Home">
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          {video && <div className={styles.card}><video
+            controls
+            width="500"
+            src={URL.createObjectURL(video)}>
+          </video></div>}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <center><input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} /></center>
+          <center><h1>Result</h1></center>
+          <center><button className={styles.button} onClick={convertToGif}>Convert</button></center>
+          {gif && <div className={styles.card}><img src={gif} width="500" /></div>}
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </main>
     </div>
-  )
+  ) : (<p>Loading...</p>);
 }
